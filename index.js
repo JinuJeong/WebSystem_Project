d5984e616e578197899c98e971722c2086631b3b
wooil <boxer1532@ajou.ac.kr>
2018-11-03 13:41:57 +0900

    commit test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const router_login = require('./router/login')
+const express = require('express');
+const app = express();
+const router_login = require('./router/login');
 
 app.set('views',__dirname +"/views");
 app.set('view engine','ejs');
