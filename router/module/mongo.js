ec6627f1572f276985250c3018f6de3179685c81
jinwooJeong <jjw8967@ajou.ac.kr>
2018-11-07 12:58:47 +0900

    Add student update&delete operation at Mongoose module

--- router/module/mongo.js
+++ router/module/mongo.js
@@ -10,38 +10,63 @@ const UserSchema = mongoose.Schema({
 //Set User Colletion
 const User = mongoose.model("User",UserSchema);
 
-exports.std_save = function(info){
-            
-				mongoose.connect(config.dbUrl());
+
+//Set mongoose connect
+function con(){
+	             mongoose.connect(config.dbUrl());
             const db =mongoose.connection;
             db.on('error',console.error.bind(console,'connection error:'));
             db.once('open',()=>{
                     console.log("connect success!!");
                     });
+}
 
-            //creat student Object
-            const student = new User(info);
+//Create Student
+exports.std_save = function(info){
+	 
+	 con();
+	 //creat student Object
+	 const student = new User(info);
 
-            student.save((err)=>{
-                    if(err) console.log(err);
-                    mongoose.disconnect();
-                    return 1;
-            })
+	 student.save((err)=>{
+				if(err) console.log(err);
+				mongoose.disconnect();
+				return 1;
+	 })
 
 }
 
+//Find Student
 exports.std_find = function(info){
-	 mongoose.connect(config.dbUrl());
-	 const db = mongoose.connection;
-	 db.on('error', console.error.bind(console,'connection error:'));
-	 db.once('open',()=>{
-				console.log("connect success!!");
-				});
+	 con();
 	 User.find(info, (err,doc)=>{
 					 console.log(doc);
+					 mongoose.disconnect();
 					 if(doc.length==0) return -1;
 
 					 else return doc;
 	 });
 					 
 }
+
+//Delete Student
+exports.std_delete = function(info){
+	 con();
+	 User.remove(info,(err,doc)=>{
+					 console.log(doc);
+					 mongoose.disconnect();
+					 return 1;
+	 });
+
+}
+
+//Update student
+exports.std_update = function(info,set){
+	 con();
+	 User.update(info,set,(err,doc)=>{
+					 console.log(doc);
+					 mongoose.disconnect();
+					 return 1;
+	 });
+}
+
