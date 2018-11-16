849637dc4e1fdbcba57041c70edb48534469933a
So Hyun Seob <eric4266@ajou.ac.kr>
2018-11-16 17:41:05 +0900

    new webpack

---
+++ src/components/signup.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="not-found">
+        <!--항상 상단에 떠있는 bar-->
+        <header-bar/>
+
+        <md-empty-state
+            md-icon="devices_other"
+            md-label="page for signup"
+            md-description="회원가입을 위한 페이지 입니다.">
+        </md-empty-state>
+    </div>
+</template>
+
+<script>
+import headerBar from './header.vue'
+
+export default {
+  name: 'signup',
+
+  components: {
+    headerBar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '../components/login'
+import Home from '../components/home'
+import NotFound from '../components/404'
+import SignUp from '../components/signup'
+
+Vue.use(Router)
+
+export default new Router({
+  // '#' tag를 없애줌.
+  mode: 'history',
+
+  // router's list
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/404',
+      component: NotFound
+    },
+    {
+      path: '/signup',
+      component: SignUp
+    }
+  ]
+})
