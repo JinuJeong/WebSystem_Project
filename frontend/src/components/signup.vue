6257d512d08f82db0c5e0b2c91d4e818f8d4a816
hwangbumchul <bumchul95@naver.com>
2018-11-21 01:17:23 +0900

    Correct conflict on DB

--- frontend/src/components/signup.vue
+++ frontend/src/components/signup.vue
@@ -3,7 +3,6 @@
         <!--항상 상단에 떠있는 bar-->
         <header-bar></header-bar>
 
-<<<<<<< HEAD
         <md-empty-state
             md-icon="devices_other"
             md-label="page for signup"
@@ -33,108 +32,6 @@
         <h2>{{circle.name}}</h2>
         <h3>소속 : {{circle.party}}</h3>
         </div>
-=======
-
-        <div class="centered-container">
-
-            <md-content class="md-elevation-10">
-            <form class="vue-form" @submit.prevent="submit">
-            <md-field>
-                <label>아이디 </label>
-                <md-input v-model="id" placeholder="ID(___@ajou.ac.kr)"></md-input>
-            </md-field>
-
-            <md-field>
-                <label>패스워드 </label>
-                <md-input v-model="password" type="password" placeholder="password"></md-input>
-            </md-field>
-
-            <md-field>
-                <label>이름</label>
-                <md-input v-model="name" placeholder="name"></md-input>
-            </md-field>
-
-            <md-field>
-                <label>생년월일</label>
-                <md-input v-model="number" type="number"  placeholder="data of birth"></md-input>
-            </md-field>
-
-            <md-field>
-                <label>자기 소개</label>
-                <md-textarea v-model="textarea"></md-textarea>
-            </md-field>
-            </form>
-
-            <div class="md-layout-item">
-                <md-field>
-                    <label>소속 학과</label>
-                    <md-select v-model="department" name="department" id="department">
-                        <md-optgroup label="정보통신대학">
-                            <md-option value="software">소프트웨어학과</md-option>
-                            <md-option value="security">사이버보안학과</md-option>
-                            <md-option value="electric">전자공학과</md-option>
-                            <md-option value="media">미디어학과</md-option>
-                            <md-option value="Defense_digital">국방디지털융합학과</md-option>
-                        </md-optgroup>
-                        <md-optgroup label="공과대학">
-                            <md-option value="mechanical">기계공학과</md-option>
-                            <md-option value="Chemical">화학공학과</md-option>
-                            <md-option value="environmental">환경공학과</md-option>
-                            <md-option value="Advanced_Materials">신소재공학과</md-option>
-                        </md-optgroup>
-                        <md-optgroup label="자연과학대학">
-                            <md-option value="mathematical">수학과</md-option>
-                            <md-option value="Physics">물리학과</md-option>
-                        </md-optgroup>
-                        <md-optgroup label="경영대학">
-                            <md-option value="business">경영학과</md-option>
-                            <md-option value="e-business">e-business 학과</md-option>
-                        </md-optgroup>
-                         <md-optgroup label="인문대학">
-                            <md-option value="korean">국어국문학과</md-option>
-                            <md-option value="english">영어영문학과</md-option>
-                        </md-optgroup>
-                        <md-optgroup label="사회과학대학">
-                            <md-option value="Psychology">심리학과</md-option>
-                            <md-option value="Economics">경제학과</md-option>
-                        </md-optgroup>
-                        <md-optgroup label="의과대학">
-                            <md-option value="medicine">의예과</md-option>
-                        </md-optgroup>
-                        <md-optgroup label="간호대학">
-                            <md-option value="nursing">간호학과</md-option>
-                        </md-optgroup>
-                        <md-optgroup label="약학대학">
-                            <md-option value="Pharmacy">약학과</md-option>
-                        </md-optgroup>
-
-                    </md-select>
-                </md-field>
-            </div>
-
-            <div class="md-layout-item">
-
-
-                <md-field>
-                    <label>관심분야</label>
-                    <md-select v-model="selectedInterest" name="Interest" id="Interest" multiple>
-                        <md-option value="music">음악</md-option>
-                        <md-option value="study">공부(Study)</md-option>
-                        <md-option value="exercise">운동</md-option>
-                        <md-option value="dance">춤</md-option>
-                        <md-option value="movie">영화 & 만화</md-option>
-                        <md-option value="trip">여행</md-option>
-                        <md-option value="contest">공모전</md-option>
-                    </md-select>
-                </md-field>
-
-                <div class="actions md-layout md-alignment-center">
-                    <md-button class="md-raised md-primary md-alignment-center">가입하기</md-button>
-                    <md-button class="md-raised md-primary" href="/">홈으로</md-button>
-                </div>
-            </div>
-        </md-content>
->>>>>>> 0a6a6208730c201e499d571f5a6e7f4679e2e2b3
         </div>
     </div>
 </template>
@@ -159,7 +56,6 @@
             disabled: null,
             department : null,
 
-<<<<<<< HEAD
 export default {
   name: 'signup',
   created () {
@@ -194,12 +90,6 @@ export default {
     headerBar
   }
 }
-=======
-            selectedInterest: []
-        })
-
-    }
->>>>>>> 0a6a6208730c201e499d571f5a6e7f4679e2e2b3
 </script>
 
 <style lang="scss" scoped>
