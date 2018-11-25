b332b0d4af745db02eec50cecf3f306f23021f9b
jinwooJeong <jjw8967@ajou.ac.kr>
2018-11-25 21:08:22 +0900

    before

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -56,6 +56,7 @@ export default new Router({
             path: '/circle',
             component: Circle
         },
+        {
             path: '/timeline',
             component: Timeline
         }
