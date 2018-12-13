407398030f98b4ff5702cfd850c4d16c223537a2
So Hyun Seob <eric4266@ajou.ac.kr>
2018-12-14 02:20:52 +0900

    add dialog

--- frontend/src/components/showBoard.vue
+++ frontend/src/components/showBoard.vue
@@ -34,7 +34,33 @@
                         rows="1"
                     />
                     <v-btn flat small color="inherit" class="button" @click="_id=cmt._id; setIsCmtChanging();">댓글 수정</v-btn>
-                    <v-btn flat small color="inherit" class="button" @click="_id=cmt._id; onCmtDelete();">댓글 삭제</v-btn>
+                    <v-dialog
+                        v-model="dialog"
+                        width="500"
+                    >
+                    <v-btn flat small slot="activator" color="inherit" class="button" @click="_id=cmt._id;">댓글 삭제</v-btn>
+                    <v-card>
+                        <v-card-title
+                            class="headline blue lighten-2"
+                            primary-title
+                        >
+                        댓글 삭제
+                        </v-card-title>
+
+                        <v-card-text>
+                        정말로 삭제하시겠습니까?
+                        </v-card-text>
+
+                        <v-divider/>
+
+                        <v-card-actions>
+                        <v-spacer/>
+                        <v-btn color="primary" flat @click="dialog = false;">취소</v-btn>
+                        <v-btn color="primary" flat @click="dialog = false; onCmtDelete();">삭제</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                    </v-dialog>
+                    
                 </div>
                 <div v-else>
                     <v-textarea
@@ -83,7 +109,8 @@
                 recovery: "",
                 cmts: [],
                 _id: "",
-                isCmtChanging: ""
+                isCmtChanging: "",
+                dialog: false
             }
         },
         created: function(){
